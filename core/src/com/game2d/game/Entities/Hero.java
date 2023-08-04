c0dd041d26d45031cfc08c84b5f8fefa7a50bad3
jeremy46081 <85791464+jeremy46081@users.noreply.github.com>
2023-08-04 17:02:03 -0600

    made changes

--- core/src/com/game2d/game/Entities/Hero.java
+++ core/src/com/game2d/game/Entities/Hero.java
@@ -1,5 +1,6 @@
 package com.game2d.game.Entities;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -7,7 +8,7 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.game2d.game.Game2D;
 
-public class Hero {
+public class Hero extends Sprite {
     public World world;
     public Body body;
     public Hero (World world){
@@ -23,7 +24,7 @@ public class Hero {
         body = world.createBody(bodyDef);
         FixtureDef fixtureDef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(9/Game2D.PPM);
+        shape.setRadius(10000000/Game2D.PPM);
         fixtureDef.shape=shape;
         body.createFixture(fixtureDef);
 
