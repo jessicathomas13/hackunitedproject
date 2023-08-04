65d51e4c4c62b9d53dadbc181989384f6afd80a7
jess79336 <127780039+jess79336@users.noreply.github.com>
2023-08-04 15:15:24 -0600

    Created libgdx project

---
+++ android/src/com/game2d/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.game2d.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.game2d.game.Game2D;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game2D(), config);
+	}
+}
---
+++ desktop/src/com/game2d/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.game2d.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.game2d.game.Game2D;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Game2D");
+		new Lwjgl3Application(new Game2D(), config);
+	}
+}
