c4d5bdcae01a817d3338b0020106b77ca1f6ad7c
jess79336 <127780039+jess79336@users.noreply.github.com>
2023-08-04 15:18:27 -0600

    render method

--- core/src/com/game2d/game/Game2D.java
+++ core/src/com/game2d/game/Game2D.java
@@ -7,25 +7,22 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game2D extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
