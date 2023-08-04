cfe03013a38479d543da63ef3988ce9d11d13bda
jeremy46081 <85791464+jeremy46081@users.noreply.github.com>
2023-08-04 15:29:42 -0600

    created player screen

--- core/src/com/game2d/game/Game2D.java
+++ core/src/com/game2d/game/Game2D.java
@@ -1,11 +1,12 @@
 package com.game2d.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Game2D extends ApplicationAdapter {
+public class Game2D extends Game {
 	SpriteBatch batch;
 
 	
