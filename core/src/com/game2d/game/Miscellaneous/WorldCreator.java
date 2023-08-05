c3fb80b57340ce741249de85862941df95980a5f
jeremy46081 <85791464+jeremy46081@users.noreply.github.com>
2023-08-05 12:03:04 -0600

    added new level

--- core/src/com/game2d/game/Miscellaneous/WorldCreator.java
+++ core/src/com/game2d/game/Miscellaneous/WorldCreator.java
@@ -32,7 +32,7 @@ public class WorldCreator {
             body.createFixture(fixtureDef);
         }
 
-        for(MapObject mapObject : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+        for(MapObject mapObject : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
             Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
             bodyDef.type = BodyDef.BodyType.StaticBody;
             bodyDef.position.set((rectangle.getX() + rectangle.getWidth()/2)/ Game2D.PPM, (rectangle.getY() + rectangle.getHeight()/2)/Game2D.PPM);
