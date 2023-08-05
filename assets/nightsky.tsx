c8d6bab184b4a53ab5c9d414fb3bed9af2151615
jeremy46081 <85791464+jeremy46081@users.noreply.github.com>
2023-08-05 12:05:42 -0600

    added new level

--- assets/nightsky.tsx
+++ assets/nightsky.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="nightsky" tilewidth="85" tileheight="85" tilecount="264" columns="22">
- <image source="../../../Downloads/Full Moon - background.png" width="1920" height="1080"/>
+ <image source="Full Moon - background.png" width="1920" height="1080"/>
 </tileset>
